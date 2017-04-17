bb32dba5c5a61f892ba9ae85eb161daf727c96b0
Megasonic988 <weixiang@ualberta.ca>
2017-04-17 13:13:55 -0600

    basic client

--- client/app.js
+++ client/app.js
@@ -1,2 +1,35 @@
 const io = require('socket.io-client');
 const socket = io();
+const PIXI = require('pixi.js');
+
+const stage = new PIXI.Container();
+const renderer = PIXI.autoDetectRenderer(512, 512);
+document.body.appendChild(renderer.view);
+
+let player;
+
+function drawPlayer(x, y) {
+  let p = new PIXI.Graphics();
+  p.beginFill(0xd77466);
+  p.drawCircle(0, 0, 32);
+  p.endFill();
+  p.x = x;
+  p.y = y;
+  return p;
+}
+
+function setup() {
+
+  player = drawPlayer(256, 256);
+  stage.addChild(player);
+
+  gameLoop();
+}
+
+function gameLoop() {
+  requestAnimationFrame(gameLoop);
+
+  renderer.render(stage);
+}
+
+setup();
--- server/app.js
+++ server/app.js
@@ -3,32 +3,83 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const players = [];
 let idCount = 0;
 
-class Player {
-  constructor(id, socket) {
-    this.id = id;
-    this.socket = socket;
-    this.connected = true;
+const GameState = {
+	players: [],
+	projectiles: []
+};
 
-    socket.on('disconnect', () => {
-      this.connected = false;
-    });
-  }
+class Vector {
+	constructor(x, y) {
+		this.x = x || 0;
+		this.y = y || 0;
+	}
+}
+
+class Player {
+	constructor(id, socket) {
+		this.id = id;
+		this.socket = socket;
+		this.connected = true;
+		this.position = new Vector();
+		this.playerInput;
+		this.lastFiredProjectileTime;
+
+		socket.on('disconnect', () => {
+			this.connected = false;
+		});
+
+		socket.on('input', input => {
+			this.lastPlayerInput = input;
+		});
+	}
+
+	handlePlayerInput() {
+
+	}
+}
+
+class Projectile {
+	constructor(x, y, vx, vy) {
+		this.position = new Vector(x, y);
+		this.velocity = new Vector(vx, vy);
+	}
 }
 
 io.on('connection', function(socket) {
-  players.push(new Player(idCount, socket));
-  idCount += 1;
+	GameState.players.push(new Player(socket.id, socket));
+	idCount += 1;
 });
 
 app.use(express.static(__dirname + '/../client'));
 
+function formatGameState() {
+	return {
+		players: GameState.players.map(p => {
+      return {
+				id: p.id,
+				position: p.position
+			};
+    }),
+		projectiles: GameState.projectiles.map(p => {
+      return {
+				position: p.position
+			};
+    })
+	};
+}
+
+setInterval(function() {
+	GameState.players = GameState.players.filter(p => p.connected);
+	GameState.players.forEach(function(p) {
+		p.socket.emit('gameState', formatGameState());
+		p.handlePlayerInput();
+	});
+}, 33);
+
 http.listen(8080, function() {
-  console.log("Server listening on port 8080.");
+	console.log("Server listening on port 8080.");
 });
 
-module.exports = {
-  players: players
-};
+module.exports = GameState;
